2da43e8c8066b61dfa29116c28aa88de70519315
Vojtěch Nevřela <vojtech@nevrela.dev>
2025-06-04 22:51:16 +0200

    Updated rotary encoder code

--- src/gpio/rotary_encoder.py
+++ src/gpio/rotary_encoder.py
@@ -1,34 +1,31 @@
 class RotaryEncoder:
-    def __init__(self, pin_a, pin_b):
+    def __init__(self, pin_a, pin_b, max=None, min=None, start=0):
         import RPi.GPIO as GPIO
         self.GPIO = GPIO  # Store GPIO as an instance attribute
         self.pin_a = pin_a
         self.pin_b = pin_b
-        self.position = 0
+        self.max = max
+        self.min = min
+        self.position = start
 
         self.GPIO.setmode(self.GPIO.BCM)
-        self.GPIO.setup(pin_a, self.GPIO.IN, pull_up_down=self.GPIO.PUD_UP)
-        self.GPIO.setup(pin_b, self.GPIO.IN, pull_up_down=self.GPIO.PUD_UP)
+        self.GPIO.setup(pin_a, self.GPIO.IN)
+        self.GPIO.setup(pin_b, self.GPIO.IN)
 
-        self.last_a = self.GPIO.input(pin_a)
 
-        self.GPIO.add_event_detect(pin_a, self.GPIO.BOTH, callback=self.update_position)
-        self.GPIO.add_event_detect(pin_b, self.GPIO.BOTH, callback=self.update_position)
+        self.GPIO.add_event_detect(pin_a, self.GPIO.RISING, callback=self.update_position)
 
     def update_position(self, channel):
-        a = self.GPIO.input(self.pin_a)
         b = self.GPIO.input(self.pin_b)
+        if b:
+            self.position += 1
+        else:
+            self.position -= 1
 
-        if a != self.last_a:
-            if b != a:
-                self.position += 1
-            else:
-                self.position -= 1
-
-        self.last_a = a
+        if self.max is not None and self.position > self.max:
+            self.position = self.max
+        if self.min is not None and self.position < self.min:
+            self.position = self.min
 
     def read_position(self):
         return self.position
-
-    def setup(self):
-        pass  # GPIO setup is already handled in __init__
--- src/main.py
+++ src/main.py
@@ -3,27 +3,44 @@ import time
 from sound.crossfade import Crossfade
 from gpio.rotary_encoder import RotaryEncoder
 
+def print_tui_table(**kwargs):
+    """
+    Prints the provided arguments in a table format, overwriting the terminal.
+    :param kwargs: Keyword arguments where keys are argument names and values are their values.
+    """
+    print("\033c", end="")  # Clear the terminal
+    print(f"{'Argument':<20} | {'Value':<20}")
+    print("-" * 43)
+    for key, value in kwargs.items():
+        print(f"{key:<20} | {value:<20}")
+
 def main():
     # Initialize GPIO
     GPIO.setmode(GPIO.BCM)
     
-    # Setup rotary encoder with GPIO pins 17 and 18
-    encoder = RotaryEncoder(pin_a=17, pin_b=18)
-    encoder.setup()
-
+    # Setup sound select encoder with GPIO pins 17 and 18
+    sound_select_encoder = RotaryEncoder(pin_a=17, pin_b=18)
+    
+    # Setup volume encoder with GPIO pins 27 and 22
+    volume_encoder = RotaryEncoder(pin_a=27, pin_b=22, max=100, min=0, start=50)
+    
     # Initialize sound crossfade
     sound_player = Crossfade()
 
     try:
         while True:
-            position = encoder.read_position()
-            print(f"Encoder Position: {position}")
+            sound_select_knob = sound_select_encoder.read_position()
+            volume_knob = volume_encoder.read_position()
+            print_tui_table(
+                sound_select_knob=sound_select_knob,
+                volume_knob=volume_knob
+            )
             # Here you would add logic to trigger sound playback based on position
             time.sleep(0.01)
     except KeyboardInterrupt:
         pass
     finally:
-        GPIO.cleanup()
+        GPIO.cleanup()  # cleanup GPIO resources
 
 if __name__ == "__main__":
     main()
