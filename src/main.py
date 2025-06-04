878224dbaa1533a651306e189798fe0149b52d0a
Vojtěch Nevřela <vojtech@nevrela.dev>
2025-06-04 23:32:05 +0200

    Implement sound playback and crossfade functionality with rotary encoder controls WIP

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import os
+import sys
 from sound.crossfade import Crossfade
 from gpio.rotary_encoder import RotaryEncoder
 
@@ -27,16 +29,36 @@ def main():
     # Initialize sound crossfade
     sound_player = Crossfade()
 
+    sound_files = sys.argv[1:]
+    if not sound_files:
+        print("No sound files provided.")
+        return
+    current_index = 0
+    last_select_position = sound_select_encoder.read_position()
+    sound_player.play_sound(sound_files[current_index])
+
+    last_volume = None
+
     try:
         while True:
             sound_select_knob = sound_select_encoder.read_position()
             volume_knob = volume_encoder.read_position()
             print_tui_table(
                 sound_select_knob=sound_select_knob,
-                volume_knob=volume_knob
+                volume_knob=volume_knob,
+                current_sound=sound_files[current_index]
             )
-            # Here you would add logic to trigger sound playback based on position
-            time.sleep(0.01)
+            if last_volume != volume_knob:
+                os.system(f"amixer sset 'PCM' {volume_knob}% > /dev/null 2>&1")
+                last_volume = volume_knob
+            if sound_select_knob != last_select_position:
+                direction = 1 if sound_select_knob > last_select_position else -1
+                new_index = current_index + direction
+                if 0 <= new_index < len(sound_files):
+                    sound_player.crossfade(sound_files[current_index], sound_files[new_index], duration=2)
+                    current_index = new_index
+                last_select_position = sound_select_knob
+            time.sleep(0.5)
     except KeyboardInterrupt:
         pass
     finally:
--- src/sound/crossfade.py
+++ src/sound/crossfade.py
@@ -1,11 +1,28 @@
+import pygame
+
 class Crossfade:
     def __init__(self):
-        pass
+        pygame.mixer.init()
+        self.current_channel = pygame.mixer.Channel(0)
+        self.next_channel = pygame.mixer.Channel(1)
+        self.current_sound = None
+        self.current_file = None
 
     def play_sound(self, sound_file):
-        # Code to play the sound file
-        pass
+        if self.current_file == sound_file:
+            return
+        sound = pygame.mixer.Sound(sound_file)
+        self.current_channel.play(sound, fade_ms=500)
+        self.current_sound = sound
+        self.current_file = sound_file
 
     def crossfade(self, sound_file_1, sound_file_2, duration):
-        # Code to crossfade between sound_file_1 and sound_file_2 over the specified duration
-        pass
+        if self.current_file != sound_file_1:
+            self.play_sound(sound_file_1)
+        next_sound = pygame.mixer.Sound(sound_file_2)
+        fade_ms = int(duration * 1000)
+        self.current_channel.fadeout(fade_ms)
+        self.next_channel.play(next_sound, fade_ms=fade_ms)
+        self.current_channel, self.next_channel = self.next_channel, self.current_channel
+        self.current_sound = next_sound
+        self.current_file = sound_file_2
